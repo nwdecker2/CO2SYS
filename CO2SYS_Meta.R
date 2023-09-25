7a1f836b1da6f67d91c9057f9bbd52a6d22eed50
nwdecker2 <deckern@myumanitoba.ca>
2023-09-25 15:34:33 -0500

    Metadata for packages used for CO2SYS in R

---
+++ CO2SYS_Meta.R
@@ -0,0 +1,12 @@
+## Information on the package used can be found here
+https://jpgattuso.github.io/seacarb.html
+
+
+## Package can be Cited as:
+## Gattuso, J.-P., Epitalon, J.-M. Lavigne H., & Orr J., 2021. seacarb: seawater
+##    carbonate chemistry. R package version 3.3.0.
+##    http://CRAN.R-project.org/package=seacarb.
+
+
+## install.packages("seacarb")
+library(seacarb)
